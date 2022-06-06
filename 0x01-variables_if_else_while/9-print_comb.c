2b143d077780313e647bf7981ee17f6d114ab561
Maxby225 <bileymax6@gmail.com>
2022-06-06 15:34:13 -0700

    task9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Program entry point
+ *
+ * Return: 0 success. Error code otherwise
+ */
+int main(void)
+{
+int n;
+for (n = 0; n < 10; n++)
+{
+if (n != 9)
+{
+putchar(n % 10 + '0');
+putchar(',');
+putchar(' ');
+}
+else
+{
+putchar(n % 10 + '0');
+}
+}
+printf("\n");
+return (0);
+}
