2f0ff0d016cf3ab8130f932f161bdb0ffbf508ff
Maxby225 <bileymax6@gmail.com>
2022-06-06 12:57:01 -0700

    task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Program Entry point
+ *
+ * Return: 0 on success. Error code otherwise
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0 ; n < 10 ; n++)
+	{
+		printf("%d", n);
+	}
+		printf("\n");
+	return (0);
+}
