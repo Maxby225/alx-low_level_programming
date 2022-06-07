e8bf7f0558ad2e0c5865af073af6b0e98c8112c4
Maxby225 <bileymax6@gmail.com>
2022-06-06 17:32:38 -0700

    task11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Program entry point
+ *
+ * Return: 0 success. Error code otherwise
+ */
+int main(void)
+{
+
+	int n, e, a;
+
+
+	for (n = 48; n < 58; n++)
+	{
+		for (n = 49; n < 58; n++)
+        {
+	       	for (a = 50 ; a < 58 ; a++)
+                {
+			if (a > e &&
+                        putchar(e);
+                        putchar(n);
+                        if (n == 57 && e == 56)
+                        {
+                                break;
+                        }
+                        putchar(',');
+                        putchar(' ');
+                }
+                n++;
+        }
+        e++;
+}
+printf("\n");
+return (0);
+}
