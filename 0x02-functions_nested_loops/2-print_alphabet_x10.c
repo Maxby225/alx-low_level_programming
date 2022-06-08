6ed364887e2c86ca8e40650d7fcf5edb970be451
Maxby225 <bileymax6@gmail.com>
2022-06-07 21:28:14 -0700

    task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet lowercase
+ *
+ * Return: Always 0.
+*/
+
+void print_alphabet_x10(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+	_putchar(alphabet*10);
+}
+
+	_putchar('\n');
+
+}
