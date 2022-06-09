c267567a1151efb29bd1bcf832c4d358c6e43831
Maxby225 <bileymax6@gmail.com>
2022-06-09 14:03:34 -0700

    debugger

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,6 +3,19 @@
 
 #include <stdio.h>
 
-void positive_or_negative(int i);
-
+void positive_or_negative(int i)
+{
+	if (i > 0)
+	{
+		printf("ùis positive\n");
+	}
+	else if (i == 0)
+	{
+		printf("is zero\n");
+	}
+	else (i < 0)
+	{
+		printf("is negative\n");
+	}
+}
 #endif /* MAIN_H */
