69ef0cdf7cde45360616d905d4c111b1b8834025
Maxby225 <bileymax6@gmail.com>
2022-06-10 22:17:03 -0700

    task5

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,12 +8,16 @@ void print_most_numbers(void)
 {
 	int n;
 
-	for (n = '0', n < 10, n++)
+	n = 0;
+	while (n < 10)
 	{
 		if (n != 2 && n != 4)
 		{
-			_putchar(n);
+			_putchar(n + '0');
 		}
-	_putchar('\n');
+
+		n++;
 	}
+
+	_putchar('\n');
 }
