e2c30e64491670090af53bd4dbdf9a54dd395c66
Maxby225 <bileymax6@gmail.com>
2022-06-14 07:31:48 -0700

    string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,17 +1,17 @@
 #include "max.h"
 /**
  * _strlen - returns the length of a string
- *
  * @s: string
- * Return :Integer the length of a string
+ * Return: Integer that is the length of a string
  */
 int _strlen(char *s)
 {
-	int longer = 0;
+	int len = 0;
+
 	while (*s != '\0')
 	{
 		s++;
-		longer++;
+		len++;
 	}
-	return (longer);
+	return (len);
 }
