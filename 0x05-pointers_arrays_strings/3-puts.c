b1a000a2f52fc1767303e1cb342e8dc7e1edb1e1
Maxby225 <bileymax6@gmail.com>
2022-06-14 07:44:01 -0700

    string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "max.h"
+/**
+ * _puts - pritns a string followed by a new line
+ * @str: streng variable
+ * Return: Nothing .
+ */
+void _puts(char *str)
+{
+	char m;
+	char str[] = m;
+
+	_putchar('\n');
+}
