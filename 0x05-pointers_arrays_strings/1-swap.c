76e39e6966a29b1e8198ecc7eb29e009a91d7c84
Maxby225 <bileymax6@gmail.com>
2022-06-14 01:00:47 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include <max.h>
+/**
+ * swap_int - swaps the values of two integers
+ * @a: first value
+ * @b: second value
+ * Return: nothing
+ */
+void swap_int(int *a, int *b)
+{
+	int n = *a;
+	*a = *b;
+	*b = n;
+}
