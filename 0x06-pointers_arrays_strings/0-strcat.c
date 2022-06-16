dfea0b069205d6efe9e0e198dc517fb93d111026
Maxby225 <bileymax6@gmail.com>
2022-06-16 15:45:10 -0700

    task0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,5 @@
 #include <main.h>
+#include <string.h>
 /**
  * _strcat - fonction that concatenates
  * two strings.
@@ -14,12 +15,16 @@ char *_strcat(char *dest, char *src)
 
 	a = 0;
 	/*find the size of dest array*/
-	while (dest[c])
-	c++;
-	/* iterate trough each src array value without the null byte*/
-	for (b = 0; src[b]; b++)
+	while (dest[a])
+		a++;
+	/*iterate trough each src array value without the null byte*/
+	for (b = 0; src[b] != 0; b++)
 	/* apprend src[b] to dest[a] while over writting the null byte in dest*/
-	dest[a++] = src[b];
+	{
+		dest[a++] = src[b];
+		a += 1;
+	}
+	dest[a] = '\0' ;
 	return (dest);
 }
 
