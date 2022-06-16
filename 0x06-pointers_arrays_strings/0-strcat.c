7cfd56b492c47eaf567bfb498e72894a789ee6cf
Maxby225 <bileymax6@gmail.com>
2022-06-16 15:52:58 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,17 +11,14 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int a, b;
+	int a = 0, b;
 
-	a = 0;
-	/*find the size of dest array*/
 	while (dest[a])
 		a++;
-	/*iterate trough each src array value without the null byte*/
+
 	for (b = 0; src[b] != 0; b++)
-	/* apprend src[b] to dest[a] while over writting the null byte in dest*/
 	{
-		dest[a++] = src[b];
+		dest[a] = src[b];
 		a += 1;
 	}
 	dest[a] = '\0';
